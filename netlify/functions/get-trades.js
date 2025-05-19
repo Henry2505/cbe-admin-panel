2b9f2be2bf5eebb2e265086caddffeff500bfebd
Henry2505 <chukwuemekahenry25052004@gmail.com>
2025-05-19 14:15:50 +0100

    Create get-trades.js

---
+++ netlify/functions/get-trades.js
@@ -0,0 +1,24 @@
+// netlify/functions/get-trades.js
+const fetch = require('node-fetch');
+
+exports.handler = async () => {
+  try {
+    const res = await fetch(
+      `https://api.jsonbin.io/v3/b/${process.env.TRADES_BIN_ID}/latest`,
+      {
+        method: 'GET',
+        headers: {
+          'X-Master-Key': process.env.JSONBIN_KEY,
+        },
+      }
+    );
+    if (!res.ok) throw new Error(`Status ${res.status}`);
+    const { record } = await res.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(record),
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.message };
+  }
+};
